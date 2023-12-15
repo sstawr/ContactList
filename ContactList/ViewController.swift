8a2437a958e35f74dea435e243ee82141020861d
Evgeni Glushko <sstawr@gmail.com>
2023-12-16 01:27:44 +0300

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Evgeni Glushko on 16.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
