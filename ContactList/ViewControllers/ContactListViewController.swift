6b6bef0d756de431f9960f1cfeaae797c9493632
Evgeni Glushko <sstawr@gmail.com>
2023-12-16 01:35:00 +0300

    Initial Commit

---
+++ ContactList/ViewControllers/ContactListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ContactListViewController.swift
+//  ContactList
+//
+//  Created by Evgeni Glushko on 16.12.23.
+//
+
+import UIKit
+
+final class ContactListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+}
